3ec249229c92800ed96b64f9c2d2e8af352bb347
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2025-05-13 16:56:19 -0300

    # Troca o tema do nvim

--- nvim/nvim/lua/plugins/tema.lua
+++ nvim/nvim/lua/plugins/tema.lua
@@ -1,6 +1,14 @@
 return {
   {
-   "scottmckendry/cyberdream.nvim",
-   config = function() vim.cmd.colorscheme "cyberdream" end  
-  },
+    "morhetz/gruvbox",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark"                      
+      vim.g.gruvbox_contrast_dark = "hard"          
+      vim.g.gruvbox_enable_italic = true          
+      vim.cmd.colorscheme("gruvbox")             
+      vim.cmd("highlight Normal guibg=#16181A")
+    end
+  }
 }
