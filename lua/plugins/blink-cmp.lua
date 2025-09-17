3e49a63df87138fbbbb62b66b169899f7b880753
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2025-09-17 17:14:40 -0300

    Adiciona novas teclas de atalho do blink

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,42 +1,59 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    dependencies = {
+      'rafamadriz/friendly-snippets'
+    },
+
     version = '1.*',
 
     opts = {
-      keymap = { 
+      keymap = {
         preset = 'enter',
         ['<C-k>'] = { 'select_prev', 'fallback' },
         ['<C-l>'] = { 'select_next', 'fallback' },
-    },
+        ['<C-o>'] = { 'show_documentation'},
+        ['<C-i>'] = { 'hide_documentation'},
+      },
 
       appearance = {
         nerd_font_variant = 'mono',
       },
 
-      completion = { 
+      completion = {
         menu = {
           auto_show = false
         },
-        documentation = { auto_show = true },
+
+        documentation = {
+          auto_show = true
+        },
+
         ghost_text = {
           enabled = true,
           show_with_selection = true,
           show_without_selection = false,
           show_with_menu = false,
           show_without_menu = true,
-        }
+        },
       },
-      signature = {enabled = true},
+
+      signature = {
+        enabled = true
+      },
 
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
 
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      fuzzy = {
+        implementation = "prefer_rust_with_warning" 
+      }
     },
 
-    opts_extend = { "sources.default" }
+    opts_extend = {
+      "sources.default"
+    }
   }
 }
