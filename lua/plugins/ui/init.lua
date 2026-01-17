aff7f18ec13227dbb43e53711e5ed32f9ca2bc8b
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-17 12:36:16 -0300

    feat: adicionar plugin noise

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -4,5 +4,6 @@ return {
   require("plugins.ui.lual-line"),
   require("plugins.ui.flash"),
   require("plugins.ui.todoComments"),
-  require("plugins.ui.which-key")
+  require("plugins.ui.which-key"),
+  require("plugins.ui.noise")
 }
