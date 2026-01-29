0ef240e8c755d0c9498d8f5c140ebc062f3a6278
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-29 11:34:07 -0300

    feat(noice): add filter for neo-tree notifications

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -8,7 +8,7 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   config = function()
     require("noice").setup({
@@ -19,11 +19,20 @@ return {
           ["cmp.entry.get_documentation"] = true,
         },
       },
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Invalid mapping"
+          },
+          opts = { skip = true },
+        },
+      },
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
-        inc_rename = false,
+        inc_rename = true,
         lsp_doc_border = false,
       },
     })
