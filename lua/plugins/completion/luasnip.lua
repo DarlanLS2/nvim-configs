855c2e5bf34634a4c5f86614f07216071c11784c
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-08-02 00:21:39 -0300

    feat(completion): organize customized  snippets

--- lua/plugins/completion/luasnip.lua
+++ lua/plugins/completion/luasnip.lua
@@ -1,8 +1,8 @@
+-- Para adicionar novos snippets basta criar uma pasta no snippets/ e dar um
+-- require aqui juntos aos outros.
 return {
 	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
+	version = "v2.*", 
   build = "make install_jsregexp",
 
   config = function()
@@ -10,17 +10,7 @@ return {
 
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    local s = ls.snippet
-    local t = ls.text_node
-    local i = ls.insert_node
-
-    ls.add_snippets("c", {
-      s("main", {
-        t({"After expanding, the cursor is here ->"}), i(1),
-        t({"", "After jumping forward once, cursor is here ->"}), i(2),
-        t({"", "After jumping once more, the snippet is exited there ->"}), i(0),
-      }),
-    })
-
+    -- Adicionar os requires aqui:
+    require("snippets.c")
   end,
 }
---
+++ lua/snippets/c.lua
@@ -0,0 +1,18 @@
+local ls = require("luasnip")
+
+local fmt = require("luasnip.extras.fmt").fmt
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+ls.add_snippets("c", {
+  s("main", fmt([[
+#include <stdio.h>
+
+int  main({}) {{
+  {}
+}}
+]], {i(1), i(0)})
+  )
+})
