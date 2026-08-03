f2f7dede934030d044bbbb192090accb8c53ff72
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-08-02 23:41:14 -0300

    fix: typo

--- lua/plugins/syntax/surround.lua
+++ lua/plugins/syntax/surround.lua
@@ -1,27 +1,27 @@
+-- KEYMAPS::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+--
+-- Basics-----------------------------------------------------------------
+-- ys | entra no modo add surround
+-- ds | entra no modo delete surround
+-- cs | entra no modo change surround
+--
+-- Inserindo surround caracter--------------------------------------------
+-- ysiw" | example -> "example"
+-- yss"  | entire line -> "entire line"
+-- ysa"( | "example" -> ("example")
+--
+-- Deletando surround caracter--------------------------------------------
+-- > ds" | "example" -> example
+-- > ds( | (example) -> example
+--
+-- Mudando o surround caracter--------------------------------------------
+-- > cs"' | "example" -> 'example'
+-- > cs[{ | [example] -> {example}
+
 return {
   "kylechui/nvim-surround",
-  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  version = "^3.0.0",
   event = "VeryLazy",
-
-  -- keymaps:
-  --
-  -- Basics-----------------------------------------------------------------
-  -- ys | entra no modo add surround
-  -- ds | entra no modo delete surround
-  -- cs | entra no modo change surround
-
-  -- Inserindo surround caracter--------------------------------------------
-  -- ysiw" | example -> "example"
-  -- yss"  | entire line -> "entire line"
-  -- ysa"( | "example" -> ("example")
-
-  -- Deletando surround caracter--------------------------------------------
-  -- > ds" | "example" -> example
-  -- > ds( | (example) -> example
-
-  -- Mudando o surround caracter--------------------------------------------
-  -- > cs"' | "example" -> 'example'
-  -- > cs[{ | [example] -> {example}
 
   config = function()
     require( "nvim-surround" ).setup({
