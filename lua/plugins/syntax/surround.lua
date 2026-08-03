dae7d08c85c42985275e55f9afd2321fff9cdc55
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-08-02 23:33:15 -0300

    feat(syntax): add usage notes to surround

--- lua/plugins/syntax/surround.lua
+++ lua/plugins/syntax/surround.lua
@@ -3,17 +3,28 @@ return {
   version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
 
-  -- keymaps interessantes:
-  -- ys -> entra no modo add surround
-  -- ds -> entra no modo delete surround
-  -- cs -> entra no modo change surround
+  -- keymaps:
   --
-  -- ysiw" -> Coloca "" em volta da palavra sobre o cursor
-  -- ds" -> Delete ""
-  -- cs"' -> Troca "" por ''
+  -- Basics-----------------------------------------------------------------
+  -- ys | entra no modo add surround
+  -- ds | entra no modo delete surround
+  -- cs | entra no modo change surround
+
+  -- Inserindo surround caracter--------------------------------------------
+  -- ysiw" | example -> "example"
+  -- yss"  | entire line -> "entire line"
+  -- ysa"( | "example" -> ("example")
+
+  -- Deletando surround caracter--------------------------------------------
+  -- > ds" | "example" -> example
+  -- > ds( | (example) -> example
+
+  -- Mudando o surround caracter--------------------------------------------
+  -- > cs"' | "example" -> 'example'
+  -- > cs[{ | [example] -> {example}
 
   config = function()
-    require("nvim-surround").setup({
+    require( "nvim-surround" ).setup({
 
     })
   end
