8315f361ce76ef6d4f414eeff3504543224d3ba1
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-08-07 20:43:57 -0300

    update(snippets): improve snippets for C language

--- lua/snippets/c.lua
+++ lua/snippets/c.lua
@@ -7,6 +7,7 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("c", {
+
   s("main", fmt([[
 #include <stdio.h>
 
@@ -14,5 +15,23 @@ int main({}) {{
   {}
 }}
 ]], {i(1), i(0)})
-  )
+  );
+
+  s("printf", fmt([[
+printf("{}");
+]], {i(0)}
+  ));
+
+  s("scanf", fmt([[
+scanf("{}", {});
+]], {i(1), i(0)}
+  ));
+
+  s("for", fmt([[
+for (int i=0; {}; i++) {{
+  {}
+}}
+]], {i(1), i(0)}
+  ));
+
 })
