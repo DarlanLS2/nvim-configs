76a011b6f667b17947df5a6ca6ad6703930de317
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-08-09 00:49:17 -0300

    feat(neo-tree): improve aparence

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -1,20 +1,36 @@
--- Este plugin é a barra de arquivos lateral
+-- Repository: https://github.com/nvim-neo-tree/neo-tree.nvim
+-- Default config: https://github.com/nvim-neo-tree/neo-tree.nvim/blob/main/lua/neo-tree/defaults.luao
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+
     branch = "v3.x",
+
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+
     lazy = false,
+
     opts = {
       default_component_configs = {
+        modified = {
+          symbol = "✎ ",
+        },
+        git_status = {
+          symbols = {
+            modified = "⚒",
+            unstaged  = "⛶",
+          }
+        },
         indent = {
           padding = 0,
-        }
+        },
       },
+
       window = {
         width = 25,
         mappings = {
@@ -29,6 +45,7 @@ return {
           ["<CR>"] = "",
         },
       },
+
       filesystem = {          -- aqui, fora do window!
         filtered_items = {
           visible = true,
@@ -36,5 +53,29 @@ return {
         },
       },
     },
+
+    config = function(_, opts)
+      require("neo-tree").setup(opts)
+
+      -- Group do background
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", {
+        bg = "#0a0a0a",
+      })
+
+      -- Group da linha selecionada
+      vim.api.nvim_set_hl(0, "NeoTreeCursorLine", {
+        bg = "#262626",
+      })
+
+      -- Group do separador de janela
+      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", {
+        fg = "#0a0a0a",
+      })
+
+      -- Group dos icons de indentação
+      vim.api.nvim_set_hl(0, "NeoTreeIndentMarker", {
+        fg = "#ff9700",
+      })
+    end
   }
 }
